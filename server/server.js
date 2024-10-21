24367516a4e0ae3f8464dfdfce3843fdf8b73656
Harleen kaur <harleen1763kaur@gmail.com>
2024-10-21 13:27:13 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,35 @@
+/*
+dr  appointment ---- 
+id:, name:string
+dr specialization:String
+//phone no hospital no , address string
+//available slot:20  */
+
+//framework configuration 
+
+
+const express = require('express')
+const connectDb=require("./config/dbConnection")
+const errorHandler=require('./middlewares/errorHandler')
+const cors=require("cors"); 
+
+connectDb();
+const app=express();
+const port =process.env.port || 5000; 
+
+
+app.use(express.json());
+app.use(cors());
+
+//routes below 
+app.get('/',(req,res)=>{
+    res.send("working");
+});
+
+//error handling middleware 
+app.use(errorHandler)
+
+//app config start 
+app.listen(port,()=>{
+    console.log(`server running on port http://localhost:${port}`);
+})

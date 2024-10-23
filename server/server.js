b8436fc28834243fb80a8062de8a8108731a8116
Harleen kaur <harleen1763kaur@gmail.com>
2024-10-23 11:30:08 +0530

    hbs

--- server/server.js
+++ server/server.js
@@ -21,6 +21,8 @@ connectDb();
 const app=express();
 const port =process.env.PORT || 5000; 
 
+app.set('view engine','hbs');
+
 
 app.use(express.json());
 app.use(cors());
@@ -28,6 +30,26 @@ app.use(cors());
 //routes below 
 app.get('/',(req,res)=>{
     res.send("working");
+    
+});
+app.get("/home",(req,res)=>{
+    res.render("home",{
+        username:"harleen",
+        posts:"flana dhimkana"
+
+    })
+})
+
+app.get("/allusers", (req, res) => {
+    // Array of user objects
+    const users = [
+        { username: "harry", age: 20 },
+        { username: "ishi", age: 22 },
+        { username: "leen", age: 25 }
+    ];
+    res.render("allusers", {
+        users: users
+    });
 });
 
 //error handling middleware 
